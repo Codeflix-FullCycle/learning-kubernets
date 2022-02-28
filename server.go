0c9b76290ac2cd24d7fc2c46a9558be3cde410ce
j-keven <jhonasnunes425@gmail.com>
2022-02-28 20:40:55 -0300

    feat: config ngix ingress

--- server.go
+++ server.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var createAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/myFamily", GetFamily)
+	http.HandleFunc("/secret", Secret)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +26,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
 }
+func Secret(w http.ResponseWriter, r *http.Request) {
 
+	user := os.Getenv("USER")
+	password := os.Getenv("password")
+	fmt.Fprintf(w, "User %s. Password %s", user, password)
+}
 func GetFamily(w http.ResponseWriter, r *http.Request) {
 
 	data, err := ioutil.ReadFile("/go/myFamily/family.txt")
@@ -31,4 +41,16 @@ func GetFamily(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "My family: %s", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(createAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
